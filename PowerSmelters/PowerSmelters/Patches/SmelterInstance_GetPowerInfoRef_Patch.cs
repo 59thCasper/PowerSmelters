24b3a27f42fa2094ab5f927e49db8922b30ed507
Casper <47537727+59thCasper@users.noreply.github.com>
2024-04-06 11:36:54 +0100

    Add files via upload

---
+++ PowerSmelters/PowerSmelters/Patches/SmelterInstance_GetPowerInfoRef_Patch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using static PowerSmelters.PowerSmelters;
+
+namespace PowerSmelters.Patches
+{
+    [HarmonyPatch(typeof(SmelterInstance))]
+    [HarmonyPatch("GetPowerInfoRef")]
+    class SmelterInstance_GetPowerInfoRef_Patch
+    {
+
+        static void Postfix(SmelterInstance __instance, ref PowerInfo __result)
+        {
+            bool isMk2 = SaveState.GetResInfoFromId(__instance.commonInfo.resId).displayName.Equals("Smelter MKII");
+            if (isMk2)
+            {
+                
+                __instance.powerInfo.UpdateSimplePowerUsage(true);
+                __instance._myDef.usesFuel = false;
+                __result.usesPowerSystem = true;
+                __result.maxPowerConsumption = 1000;
+                __result.hasPowerConnection = true;
+
+            }
+
+            bool isMk1 = SaveState.GetResInfoFromId(__instance.commonInfo.resId).displayName.Equals("Smelter");
+            if (isMk1)
+            {
+
+                __instance.powerInfo.UpdateSimplePowerUsage(true);
+                __instance._myDef.usesFuel = false;
+                __result.usesPowerSystem = true;
+                __result.maxPowerConsumption = 200;
+                __result.hasPowerConnection = true;
+
+            }
+        }
+        
+
+    }
+}
---
+++ PowerSmelters/PowerSmelters/PowerSmelters.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Voxeland5.Interface;
+
+namespace PowerSmelters
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class PowerSmelters : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.casper.PowerSmelters";
+        private const string PluginName = "PowerSmelters";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+
+        private void Awake()
+        {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+        }
+
+        private void OnGUI()
+        {
+            if (SharedState.IsInspectingSmelter && !UIManager.instance.anyMenuOpen && !Player.instance.builder.HasBuilder)
+            {
+                GUIStyle labelStyle = new GUIStyle(GUI.skin.label)
+                {
+                    normal = { textColor = Color.white },
+                    fontSize = 24
+                };
+
+                string powerConsumptionText = $"Current Power Consumption: {SharedState.CurrentPowerConsumption}W";
+
+                GUI.Label(new Rect(20, Screen.height - 100, 560, 50), powerConsumptionText, labelStyle);
+
+            }
+        }
+
+        public static class SharedState
+        {
+            public static bool IsInspectingSmelter { get; set; } = false;
+            public static int CurrentPowerConsumption { get; set; } = 0;
+        }
+    }
+}
